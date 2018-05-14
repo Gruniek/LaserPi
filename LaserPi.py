b69083d84a74ab3a6ec141f8d73d2acc1f244064
Gruniek <mous.be@gmail.com>
2018-05-14 16:25:40 +0200

    Update LaserPi.py

--- LaserPi.py
+++ LaserPi.py
@@ -2,13 +2,68 @@
 import RPi.GPIO
 import time
 
+#Pin
+
+Xstep      = 17  # Out
+Xdir       = 4   # Out
+Ystep      = 22  # Out
+Ydir       = 27  # Out
+Zstep      = 26  # Out
+Zdir       = 19  # Out
+Laser      = 18  # Out
+LedStatus  = 20  # Out
+LedError   = 21  # Out
+
+XendStop   = 5   # In
+YendStop   = 6   # In
+ZendStop   = 13  # In
+EmgStop    = 16  # In
+##############
+
+
+#Variable
+x = y = z = 0.0     # XYZ position
+
+
 # Initialisation de la numerotation et des E/S
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(19, GPIO.IN)
+GPIO.setup(Xstep, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(Xdir, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(Ystep, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(Ydir, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(Zstep, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(Zdir, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LedStatus, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LedError, GPIO.OUT, initial=GPIO.LOW)
+
+GPIO.setup(XendStop, GPIO.IN)
+GPIO.setup(YendStop, GPIO.IN)
+GPIO.setup(ZendStop, GPIO.IN)
+GPIO.setup(EmgStop, GPIO.IN)
+
+
+
+
 
 # Si on detecte un appui sur le bouton, on allume la LED 
 # et on attend que le bouton soit relache
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 while True:
@@ -21,3 +76,13 @@ while True:
             time.sleep(0.02)  # Pause pour ne pas saturer le processeur
         GPIO.output(12, GPIO.LOW)
     time.sleep(0.02)  # Pause pour ne pas saturer le processeur
+
+    
+    
+    
+def BackHome ( ):
+    # X go home
+    home = GPIO.input(XendStop)
+
+
+    retunr
